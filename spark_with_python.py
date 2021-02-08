f59b64bf6afb6dfc131b49efa9b1e04e13722654
Ankur Saxena <ankiboyz@gmail.com>
2021-02-08 14:42:09 +0530

    commit

--- spark_with_python.py
+++ spark_with_python.py
@@ -2,10 +2,14 @@
 from pyspark.sql import SparkSession
 import wget
 
-spark = SparkSession.builder.master("spark://192.168.2.221:7077").getOrCreate()
+# spark = SparkSession.builder.master("local").getOrCreate()
+# url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+# wget.download(url)
+# spark = SparkSession.builder.master("spark://192.168.2.221:7077").getOrCreate()
+spark = SparkSession.builder.master("local").getOrCreate()
+
 
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 wget.download(url)
-
 data = spark.read.csv("iris.data")
 data.show(n=5)
